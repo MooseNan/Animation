170f751a41f3c069c12bbc3d831abd0f50b13d55
Nancy <nan.li@coupang.com>
2016-12-02 16:22:43 +0800

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Li Nan on 16/12/2.
+//  Copyright © 2016年 nancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by Li Nan on 16/12/2.
+//  Copyright © 2016年 nancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
